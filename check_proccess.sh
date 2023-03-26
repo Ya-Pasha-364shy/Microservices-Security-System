fa9767fc31d0ba66c57dc5f567066f84a4c6c244
K0001rch <pv-chernov2000@yandex.ru>
2023-03-26 16:46:55 +0700

    feature: #3 checker of binaries for CI was added

---
+++ check_proccess.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+counter=0
+
+while IFS= read -r line; do
+	INR=`echo $line | grep "service" | awk '{print $9}'`
+	if [ "$INR" == "backup_service" ] || [ "$INR" == "service_manager" ]; then
+		counter=$(($counter + 1));
+	fi
+done<./test.txt
+rm test.txt
+
+if [ "$counter" -eq 2 ]; then
+	echo "OK"
+else
+	echo "Error !"
+fi
